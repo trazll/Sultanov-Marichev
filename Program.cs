ce503868b305eb1cc031d4fcbb9d642ba5759e4b
ygighk <Student_06@DESKTOP-JGRA52E>
2025-09-06 11:37:35 +0300

    1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,35 @@
 ﻿using System;
-usin
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sultanov_Marichev
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string
+            a = ("Ваша любимая игра/кино/аниме");
+            Console.WriteLine(a);
+            String b = (Console.ReadLine());
+            Console.WriteLine(b);
+            string
+            a1 = ("Ваша любимая цифра");
+            Console.WriteLine(a1);
+            string b1 = (Console.ReadLine());
+            Console.WriteLine(b1);
+            string
+            a2 = ("число пи");
+            Console.WriteLine(a2);
+            double b2 = 3.14;
+            Console.WriteLine(b2);
+            string
+            a3123 = ("Ваша любимая буква алфавита");
+            Console.WriteLine(a3123);
+            string b3123 = (Console.ReadLine());
+            Console.WriteLine(b3123);
+        } 
+    }
+}
